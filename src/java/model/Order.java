ab049d6f9cd69ccfc2203e4eb644046e58d01d28
anhqb <phamanhqb73@gmail.com>
2022-05-31 13:05:09 +0700

    All Models
    Order_status kiểu int chứ ??

---
+++ src/java/model/Order.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Date;
+
+public class Order {
+    int ID;
+    int userID;
+    int totalPrice;
+    int status;
+    Date orderDate;
+    String note;
+
+    public Order() {
+    }
+
+    public Order(int ID, int userID, int totalPrice, int status, Date orderDate, String note) {
+        this.ID = ID;
+        this.userID = userID;
+        this.totalPrice = totalPrice;
+        this.status = status;
+        this.orderDate = orderDate;
+        this.note = note;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+    
+    
+}
