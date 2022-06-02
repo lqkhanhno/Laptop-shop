984747f2f958f71dbfdc07a18cfeb4ba87989203
anhqb <phamanhqb73@gmail.com>
2022-06-02 10:09:50 +0700

    Cart
    Thêm hàm getData vào DBContext
    Tạo ProductDAO
    Cart gồm chức năng: addCart vs showCart

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -7,7 +7,9 @@ package dal;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,6 +28,18 @@ public class DBContext {
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    public ResultSet getData(String sql){
+        ResultSet rs =null;
+        try {
+            Statement state= connection.createStatement(
+                    ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+            rs=state.executeQuery(sql);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return rs;
     }
     
 }
---
+++ src/java/dal/ProductDAO.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import model.Product;
+
+/**
+ *
+ * @author LAPTOP VINH HA
+ */
+public class ProductDAO extends DBContext{
+    public Vector<Product> listAll(String sql) {
+        Vector<Product> listProduct = new Vector<>();
+        ResultSet rs = getData(sql);
+        try {
+            while (rs.next()) {
+                int productID = rs.getInt(1);
+                String productName = rs.getString(2);
+                String description = rs.getString(3);
+                int originalPrice = rs.getInt(4);
+                int categoryID = rs.getInt(5);
+                int sellPrice = rs.getInt(6);
+                int salePercent = rs.getInt(7);
+                int amount = rs.getInt(8);
+                int suppliID = rs.getInt(9);
+                Product product = new Product(productID, productName, description, 
+                        originalPrice, categoryID, sellPrice, salePercent, amount, suppliID);
+                listProduct.add(product);
+                
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return listProduct;
+    }
+    
+    public Product GetProductByID(int id){
+        Product product = new Product();
+        String query= "select * from Product where productID="+id;
+        ResultSet rs = getData(query);
+        try {
+            while (rs.next()) {
+                int productID = rs.getInt(1);
+                String productName = rs.getString(2);
+                String description = rs.getString(3);
+                int originalPrice = rs.getInt(4);
+                int categoryID = rs.getInt(5);
+                int sellPrice = rs.getInt(6);
+                int salePercent = rs.getInt(7);
+                int amount = rs.getInt(8);
+                int suppliID = rs.getInt(9);
+                product = new Product(productID, productName, description, 
+                        originalPrice, categoryID, sellPrice, salePercent, amount, suppliID);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+        return product;
+    }
+}
